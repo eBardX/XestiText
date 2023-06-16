f818b5aaa400a6237a91379aeb9263c203aba4af
John Pusey <ebardx@gmail.com>
2023-06-16 17:29:06 -0400

    Updated to support Swift 5.8 and iOS 14+

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.8
 
-// © 2018–2022 J. G. Pusey (see LICENSE.md)
+// © 2018–2023 J. G. Pusey (see LICENSE.md)
 
 import PackageDescription
 
 let package = Package(name: "XestiText",
-                      platforms: [.macOS(.v11)],
+                      platforms: [.iOS(.v14),
+                                  .macOS(.v11)],
                       products: [.library(name: "XestiText",
                                           targets: ["XestiText"])],
                       targets: [.target(name: "XestiText")],
