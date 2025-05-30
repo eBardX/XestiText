9a257101cd175455c1562a7292bbb8e6755c22e8
John Pusey <eBardX@users.noreply.github.com>
2025-05-30 17:54:12 -0400

    Update TableFormatter.Context.swift

--- Sources/XestiText/KeyValueFormat/TableFormatter.Context.swift
+++ Sources/XestiText/KeyValueFormat/TableFormatter.Context.swift
@@ -9,14 +9,12 @@ extension TableFormatter {
         // MARK: Internal Initializers
 
         internal init() {
-            self.keys = []
-            self.values = []
+            self.pairs = []
         }
 
         // MARK: Internal Instance Properties
 
-        internal private(set) var keys: [String]
-        internal private(set) var values: [String]
+        internal private(set) var pairs: [String]
     }
 }
 
@@ -27,14 +25,25 @@ extension TableFormatter.Context {
     // MARK: Internal Instance Properties
 
     internal var isEmpty: Bool {
-        keys.isEmpty && values.isEmpty
+        pairs.isEmpty
     }
 
     // MARK: Internal Instance Methods
 
-    internal mutating func append(_ key: String,
-                                  _ value: Any) {
-        keys.append(key)
-        values.append(String(describing: value))
+    internal mutating func append(_ key: String?,
+                                  _ value: Any?) {
+        let ktext = if let key {
+            Text(key, .left)
+        } else {
+            nil
+        }
+
+        let vtext = if let value {
+            Text(String(describing: value), .left)
+        } else {
+            nil
+        }
+
+        pairs.append((ktext, vtext))
     }
 }
