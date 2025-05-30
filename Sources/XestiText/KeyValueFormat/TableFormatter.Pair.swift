175d3c5239ce6c68212e13ce643fe326c1300f03
John Pusey <eBardX@users.noreply.github.com>
2025-05-30 17:43:21 -0400

    Update TableFormatter.Pair.swift

--- Sources/XestiText/KeyValueFormat/TableFormatter.Pair.swift
+++ Sources/XestiText/KeyValueFormat/TableFormatter.Pair.swift
@@ -4,25 +4,5 @@ extension TableFormatter {
 
     // MARK: Internal Nested Types
 
-    internal struct Pair: Sendable {
-
-        // MARK: Internal Initializers
-
-        internal init(_ text: String? = nil,
-                      _ alignment: String.Alignment = .center) {
-            self.alignment = alignment
-            self.lines = Self._splitIntoLines(text)
-        }
-
-        internal init(_ key: Text,
-                      _ value: Text) {
-            self.key = key
-            self.value = value
-        }
-
-        // MARK: Internal Instance Properties
-
-        internal let key: Text
-        internal let value: Text
-    }
+    internal typealias Pair = (key: Text?, value: Text?)
 }
