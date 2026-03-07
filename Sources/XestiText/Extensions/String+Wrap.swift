7e08741b7072aa8c1f37a9628e73211e0a5e755b
John Pusey <ebardx@gmail.com>
2026-03-07 15:53:47 -0500

    Fix broken string wrapping

--- Sources/XestiText/Extensions/String+Wrap.swift
+++ Sources/XestiText/Extensions/String+Wrap.swift
@@ -9,84 +9,69 @@ extension String {
         guard count > width
         else { return [self] }
 
-        let words = _splitIntoWords()
-
-        if splitWords {
-            return _arrangeSplitWords(words, width)
-        }
+        let words = _splitIntoWords(width, splitWords)
 
         return _arrangeWords(words, width)
     }
 
     // MARK: Private Instance Methods
 
-    private func _arrangeWords(_ words: [Self],
-                               _ width: Int) -> [Self] {
-        var segments: [Self] = []
-        var segment = ""
-
-        for word in words {
-            if segment.isEmpty {
-                segment = word
-            } else if segment.count + 1 + word.count > width {
-                segments.append(segment)
-
-                segment = word
-            } else {
-                segment.append(" ")
-                segment.append(word)
-            }
-        }
-
-        if !segment.isEmpty {
-            segments.append(segment)
-        }
-
-        return segments
-    }
-
-    private func _arrangeSplitWords(_ words: [Self],
-                                    _ width: Int) -> [Self] {
-        var segments: [Self] = []
-        var segment = ""
-
-        for word in words {
-            if word.count > width {
-                if !segment.isEmpty {
-                    segments.append(segment)
-
-                    segment = ""
-                }
-
-                for char in word {
-                    if segment.count + 1 > width {
-                        segments.append(segment)
-
-                        segment = Self(char)
-                    } else {
-                        segment.append(char)
-                    }
-                }
-            } else if segment.count + 1 + 1 > width {
-                segments.append(segment)
-
-                segment = word
-            } else {
-                segment.append(" ")
-                segment.append(word)
-            }
-        }
-
-        if !segment.isEmpty {
-            segments.append(segment)
-        }
-
-        return segments
-    }
-
-    private func _splitIntoWords() -> [Self] {
-        self.split { $0.isNewline || $0.isWhitespace }
+    private func _splitIntoWords(_ width: Int,
+                                 _ splitWords: Bool) -> [Self] {
+        split { $0.isWhitespace }
             .filter { !$0.isEmpty }
             .map(Self.init)
+            .flatMap { word in
+                if splitWords, word.count > width {
+                    _splitWord(word, width)
+                } else {
+                    [word]
+                }
+            }
     }
+}
+
+// MARK: - Private Functions
+
+private func _arrangeWords(_ words: [String],
+                           _ width: Int) -> [String] {
+    var segments: [String] = []
+    var segment = ""
+
+    for word in words {
+        if segment.isEmpty {
+            segment = word
+        } else if (segment.count + 1 + word.count) > width {
+            segments.append(segment)
+
+            segment = word
+        } else {
+            segment.append(" ")
+            segment.append(word)
+        }
+    }
+
+    if !segment.isEmpty {
+        segments.append(segment)
+    }
+
+    return segments
+}
+
+private func _splitWord(_ word: String,
+                        _ width: Int) -> [String] {
+    var fragments: [String] = []
+    var fragment = word
+
+    while fragment.count > width {
+        fragments.append(String(fragment.prefix(width)))
+
+        fragment = String(fragment.dropFirst(width))
+    }
+
+    if !fragment.isEmpty {
+        fragments.append(fragment)
+    }
+
+    return fragments
 }
