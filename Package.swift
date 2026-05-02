f38c62e7d00b81c2759041e1ed00bca027bbbc56
John Pusey <ebardx@gmail.com>
2026-05-02 16:42:23 -0400

    Move doc handling out of Makefile to dedicated scripts

--- Package.swift
+++ Package.swift
@@ -4,18 +4,6 @@
 
 import PackageDescription
 
-let package = Package(name: "XestiText",
-                      platforms: [.iOS(.v16),
-                                  .macOS(.v14)],
-                      products: [.library(name: "XestiText",
-                                          targets: ["XestiText"])],
-                      dependencies: [.package(url: "https://github.com/swiftlang/swift-docc-plugin.git",
-                                              .upToNextMajor(from: "1.1.0"))],
-                      targets: [.target(name: "XestiText"),
-                                .testTarget(name: "XestiTextTests",
-                                            dependencies: [.target(name: "XestiText")])],
-                      swiftLanguageModes: [.v6])
-
 let swiftSettings: [SwiftSetting] = [.defaultIsolation(nil),
                                      .enableUpcomingFeature("ExistentialAny"),
                                      .enableUpcomingFeature("ImmutableWeakCaptures"),
@@ -24,10 +12,16 @@ let swiftSettings: [SwiftSetting] = [.defaultIsolation(nil),
                                      .enableUpcomingFeature("MemberImportVisibility"),
                                      .enableUpcomingFeature("NonisolatedNonsendingByDefault")]
 
-for target in package.targets {
-    var settings = target.swiftSettings ?? []
-
-    settings.append(contentsOf: swiftSettings)
-
-    target.swiftSettings = settings
-}
+let package = Package(name: "XestiText",
+                      platforms: [.iOS(.v16),
+                                  .macOS(.v14)],
+                      products: [.library(name: "XestiText",
+                                          targets: ["XestiText"])],
+                      dependencies: [.package(url: "https://github.com/swiftlang/swift-docc-plugin.git",
+                                              .upToNextMajor(from: "1.5.0"))],
+                      targets: [.target(name: "XestiText",
+                                        swiftSettings: swiftSettings),
+                                .testTarget(name: "XestiTextTests",
+                                            dependencies: [.target(name: "XestiText")],
+                                            swiftSettings: swiftSettings)],
+                      swiftLanguageModes: [.v6])
